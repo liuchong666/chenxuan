458ddd5497e4b087c3826eb30e62ddd4b0c2d519
liuchong <chong.liu@yunfangtan.com>
2018-11-21 14:03:42 +0800

    分支测试

--- DeXinYuan/Controllers/ProductController.cs
+++ DeXinYuan/Controllers/ProductController.cs
@@ -8,7 +8,6 @@ namespace DeXinYuan.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
         public ActionResult Index()
         {
             return View();
