6564396dbd64f34f26875f4ec30629e803e73510
梅西练扣篮 <liuchong2012@hotmail.com>
2018-12-03 22:12:44 +0800

    添加分类

--- DeXinYuan/Controllers/ProductController.cs
+++ DeXinYuan/Controllers/ProductController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DeXinYuan.Models;
 
 namespace DeXinYuan.Controllers
 {
     public class ProductController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int categoryId=0)
         {
-            return View();
+            var list = SqlHelper.Query<Category>("select * from Category where IsDel=0").ToList();
+
+            var pList = SqlHelper.Query<Product>("select * from Product where IsDel=0 and CategoryId=@CategoryId", new { categoryId= categoryId==0?list[0].Id: categoryId }).ToList();
+
+            ViewBag.categoryId = categoryId == 0 ? list[0].Id : categoryId;
+            ViewBag.products = pList;
+            return View(list);
         }
     }
 }
