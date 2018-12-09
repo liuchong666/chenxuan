aa07235200e8ba97ccc845f8289ed25104f4fc92
梅西练扣篮 <liuchong2012@hotmail.com>
2018-12-09 20:41:12 +0800

    添加关于我们

--- DeXinYuan/Controllers/HomeController.cs
+++ DeXinYuan/Controllers/HomeController.cs
@@ -16,8 +16,6 @@ namespace DeXinYuan.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
