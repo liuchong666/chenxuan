28a34be0cc4f8f966c0a92424ea0286e3fb39279
梅西练扣篮 <liuchong2012@hotmail.com>
2019-04-13 21:12:34 +0800

    添加新闻页面

---
+++ DeXinYuan/Controllers/NewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeXinYuan.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewsDetail()
+        {
+            return View();
+        }
+    }
+}
