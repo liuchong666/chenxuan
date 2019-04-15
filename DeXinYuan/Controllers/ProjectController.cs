00958659ee54dc64242917fc671e2668a9b3021f
liuchong <liuchong2012@hotmail.com>
2019-04-15 15:02:36 +0800

    添加工程案例

---
+++ DeXinYuan/Controllers/ProjectController.cs
@@ -0,0 +1,23 @@
+﻿using DeXinYuan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeXinYuan.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            var list = SqlHelper.Query<Category>("select * from Category where IsDel=0").ToList();
+
+            var pList = SqlHelper.Query<Product>("select * from Product where IsDel=0 and CategoryId=@CategoryId", new { categoryId = 1}).ToList();
+
+            ViewBag.products = pList;
+            return View(list);
+        }
+    }
+}
